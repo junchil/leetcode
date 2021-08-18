5c11cdb52fbd0ef66f4c20856baf107118e21999
Steve Liu <ljc1993917@gmail.com>
2021-08-18 12:04:19 +1000

    Create main.go

---
+++ leetcodeGolang/021/main.go
@@ -0,0 +1,49 @@
+package main
+
+//Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	var head, node *ListNode
+	if l1.Val <= l2.Val {
+		node = l1
+		head = l1
+		l1 = l1.Next
+	} else {
+		node = l2
+		head = l2
+		l2 = l2.Next
+	}
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			node.Next = l1
+			l1 = l1.Next
+		} else {
+			node.Next = l2
+			l2 = l2.Next
+		}
+		node = node.Next
+	}
+
+	if l1 == nil {
+		node.Next = l2
+	}
+
+	if l2 == nil {
+		node.Next = l1
+	}
+
+	return head
+}
